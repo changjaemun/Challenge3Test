b5575c7e85aecb584213d60bf91d03cb7169c828
Ito <ckdwo23@gmail.com>
2025-05-26 11:28:34 +0900

    [Start] 초기 파일 넣음

---
+++ SongGeonTest/SongGeonTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SongGeonTest
+//
+//  Created by 문창재 on 5/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SongGeonTest/SongGeonTest/SongGeonTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongGeonTestApp.swift
+//  SongGeonTest
+//
+//  Created by 문창재 on 5/26/25.
+//
+
+import SwiftUI
+
+@main
+struct SongGeonTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
